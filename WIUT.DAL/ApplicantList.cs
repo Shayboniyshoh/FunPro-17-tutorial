ba4f071dac42fafbe9d8fb5f07da642b537c0463
Shayboniy Abduvaliev <00008699@wiut.uz>
2020-02-11 10:41:23 +0500

    17th of tutorial from FunPro

---
+++ WIUT.DAL/ApplicantList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIUT.DAL
+{
+    public class ApplicantList
+    {
+        public List<Applicant> GetAllApplicants()
+        {
+            return new ApplicantManager().GetAll();
+        }
+
+       public List<Applicant> Sort(ByAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case ByAttribute.Name:
+                    var applicants = GetAllApplicants();
+                    applicants.Sort(new ByNameComparer());
+                    return applicants;
+
+                case ByAttribute.Surname:
+                    break;
+                case ByAttribute.DoB:
+                    break;
+                case ByAttribute.Course:
+                    break;
+            }
+
+            //if we are here - something went wrong
+            return null;
+        }
+
+        private class ByNameComparer : IComparer<Applicant>
+        {
+            public int Compare(Applicant x, Applicant y)
+            {
+                //throw new NotImplementedException(); originally generated
+                return string.Compare(x.Name, y.Name);
+            }
+        }
+
+
+        public List<Applicant> SortLINQ(ByAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case ByAttribute.Name:
+                    return GetAllApplicants().OrderBy(d => d.Name).ToList(); // OrderBy for sorting
+
+                case ByAttribute.Surname:
+                    return GetAllApplicants().OrderBy(d => d.Surname).ToList();
+
+                case ByAttribute.DoB:
+                    return GetAllApplicants().OrderBy(d => d.DoB).ToList();
+
+                case ByAttribute.Course:
+                    return GetAllApplicants().OrderBy(d => d.Course).ToList();
+
+            }
+
+            //if we are here - something went wrong
+            return null;
+        }
+
+
+        public List<Applicant> Search(string value, ByAttribute attribute)
+        {
+            switch (attribute)
+            {
+                case ByAttribute.Name:
+                    return GetAllApplicants().Where(a => a.Name.ToUpper().Contains(value.ToUpper())).ToList(); // Where is for searching
+                case ByAttribute.Surname:
+                    return GetAllApplicants().Where(a => a.Surname.ToLower().Contains(value.ToLower())).ToList();
+            }
+
+            //if we are here - something went wrong
+            return null;
+        }
+
+
+
+
+    }
+}
---
+++ WIUT/ParentForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using WIUT.DAL;
+
+namespace WIUT
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = new AboutForm();
+            form.ShowDialog();
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+            // var cm = new CourseManager();
+            // MessageBox.Show(cm.GetAll().Count.ToString());
+            // MessageBox.Show(new CourseManager().GetAll().Count.ToString());
+            // MessageBox.Show(new CourseManager().GetAll().First().Name);
+
+
+
+        }
+    }
+}
